e760a0264d623973c8d5819b0d6ed3976ee546dd
guara7 <38889742+guara7@users.noreply.github.com>
2022-09-28 13:16:40 -0300

    código completado e testado com sucesso

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone n1 = new Nokia(numero: "222",modelo: "Lumia 930",imei: "111",memoria: 256);
+Console.WriteLine($"Usando {n1.Modelo}");
+n1.Ligar();
+n1.InstalarAplicativo("ZapZap");
+n1.ReceberLigacao();
+Console.WriteLine("\n");
+
+Smartphone i1 = new Iphone(numero: "444",modelo: "Iphone X",imei: "333",memoria: 128);
+Console.WriteLine($"Usando {i1.Modelo}");
+i1.Ligar();
+i1.InstalarAplicativo("OneDrive");
+i1.ReceberLigacao();
